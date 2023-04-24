428df50e55cd23672f6b23584bf3ff225b64d5de
Reallisticus <jmakin64@gmail.com>
2023-04-24 18:07:08 +0300

    playing around...

--- src/components/Earth.tsx
+++ src/components/Earth.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState, useCallback, useMemo } from "react";
+import { useEffect, useRef, useState, useCallback } from "react";
 import {
   AmbientLight,
   ClampToEdgeWrapping,
@@ -17,48 +17,70 @@ import {
   Points,
   ShaderMaterial,
   Vector3,
+  Raycaster,
+  Vector2,
+  LoadingManager,
 } from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { AiOutlinePause } from "react-icons/ai";
-import { useDynamicStyle } from "../hooks/useDynamicStyle";
-
-interface TextStyle {
-  opacity: number;
-}
+import Loader from "./Loader";
 
 const Earth = () => {
   const containerRef = useRef<HTMLDivElement | null>(null);
-  const [paused, setPaused] = useState(false);
-  const [earthTextStyle, updateEarthTextStyle] = useDynamicStyle<TextStyle>({
-    opacity: 1,
-  });
+  const earthTextRef = useRef<HTMLHeadingElement | null>(null);
+  const earthComingSoonRef = useRef<HTMLHeadingElement | null>(null);
+  const [hoveredObject, setHoveredObject] = useState<Mesh | null>(null);
+  const [cameraTarget, setCameraTarget] = useState("earth");
+  const [cameraPosition, setCameraPosition] = useState(new Vector3(0, 0, 3));
+  const [loading, setLoading] = useState(true);
 
-  const [earthComingSoonStyle, updateEarthComingSoonStyle] =
-    useDynamicStyle<TextStyle>({
-      opacity: 1,
-    });
+  const [selectedObject, setSelectedObject] = useState<Mesh | null>(null);
+
+  const [paused, setPaused] = useState(false);
+
+  const handleKey = (event: KeyboardEvent) => {
+    if (event.code === "Space") {
+      setPaused(event.type === "keydown");
+    }
+  };
 
   const handleKeyDown = (event: KeyboardEvent) => {
-    if (event.code === "Space") {
-      setPaused(true);
+    const speed = 0.1;
+    switch (event.code) {
+      case "KeyW":
+        setCameraPosition((prevPosition) =>
+          prevPosition.add(new Vector3(0, 0, -speed))
+        );
+        break;
+      case "KeyA":
+        setCameraPosition((prevPosition) =>
+          prevPosition.add(new Vector3(-speed, 0, 0))
+        );
+        break;
+      case "KeyS":
+        setCameraPosition((prevPosition) =>
+          prevPosition.add(new Vector3(0, 0, speed))
+        );
+        break;
+      case "KeyD":
+        setCameraPosition((prevPosition) =>
+          prevPosition.add(new Vector3(speed, 0, 0))
+        );
+        break;
+      default:
+        break;
     }
   };
 
-  const handleKeyUp = (event: KeyboardEvent) => {
-    if (event.code === "Space") {
-      setPaused(false);
-    }
-  };
+  // useEffect(() => {
+  //   window.addEventListener("keydown", handleKey);
+  //   window.addEventListener("keyup", handleKey);
 
-  useEffect(() => {
-    window.addEventListener("keydown", handleKeyDown);
-    window.addEventListener("keyup", handleKeyUp);
-
-    return () => {
-      window.removeEventListener("keydown", handleKeyDown);
-      window.removeEventListener("keyup", handleKeyUp);
-    };
-  }, []);
+  //   return () => {
+  //     window.removeEventListener("keydown", handleKey);
+  //     window.removeEventListener("keyup", handleKey);
+  //   };
+  // }, []);
 
   const generateRandomStarPositions = useCallback(
     (count: number, radius: number) => {
@@ -82,15 +104,35 @@ const Earth = () => {
     []
   );
 
-  const createStarMaterial = useCallback((size: number, color: string) => {
+  const generateRandomStarSizes = (
+    count: number,
+    minSize: number,
+    maxSize: number
+  ) => {
+    const sizes = new Float32Array(count);
+
+    for (let i = 0; i < count; i++) {
+      sizes[i] = Math.random() * (maxSize - minSize) + minSize;
+    }
+
+    return sizes;
+  };
+
+  const getRandomArbitrary = (min: number, max: number) => {
+    return Math.random() * (max - min) + min;
+  };
+
+  const createStarMaterial = useCallback((colors: string[]) => {
     return new ShaderMaterial({
       uniforms: {
-        size: { value: size },
-        color: { value: new Color(color) },
+        size: { value: getRandomArbitrary(0.05, 5) },
+        color: {
+          value: new Color(colors[Math.floor(Math.random() * colors.length)]),
+        },
         time: { value: 0 },
       },
       vertexShader: `
-      uniform float size;
+      attribute float size;
       uniform float time;
       varying float opacity;
 
@@ -105,15 +147,29 @@ const Earth = () => {
       varying float opacity;
 
       void main() {
-        gl_FragColor = vec4(color, 1.0) * opacity;
+        float distanceFromCenter = distance(gl_PointCoord, vec2(0.5, 0.5));
+        float edgeOpacity = 1.0 - smoothstep(0.4, 0.5, distanceFromCenter);
+        gl_FragColor = vec4(color, opacity * edgeOpacity);
       }
     `,
       transparent: true,
     });
   }, []);
 
+  const starColors = ["#ffffff", "#ffaa00", "#00aaff", "#aaaaff"];
+
   useEffect(() => {
     if (!containerRef.current) return;
+
+    const manager = new LoadingManager();
+
+    manager.onStart = () => {
+      setLoading(true);
+    };
+
+    manager.onLoad = () => {
+      setLoading(false);
+    };
 
     // Set up the scene, camera and renderer
     const scene = new Scene();
@@ -123,12 +179,14 @@ const Earth = () => {
       0.1,
       1000
     );
+    camera.position.copy(cameraPosition);
+
     const renderer = new WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     containerRef.current.appendChild(renderer.domElement);
 
     // Add your objects here
-    const textureLoader = new TextureLoader();
+    const textureLoader = new TextureLoader(manager);
 
     const earthTexture = textureLoader.load("/earthmap.jpg");
     earthTexture.wrapS = ClampToEdgeWrapping;
@@ -177,11 +235,12 @@ const Earth = () => {
     earthMoonGroup.add(earth);
     earthMoonGroup.add(clouds);
     earthMoonGroup.add(moon);
+
     scene.add(earthMoonGroup);
 
     const starCount = 1000;
-    const starSize = 0.1;
-    const starColor = "white";
+    const starMinSize = 0.05;
+    const starMaxSize = 5;
     const starRadius = 50;
 
     const starGeometry = new BufferGeometry();
@@ -193,11 +252,71 @@ const Earth = () => {
       )
     );
 
-    const starMaterial = createStarMaterial(starSize, starColor);
+    const starSizes = generateRandomStarSizes(
+      starCount,
+      starMinSize,
+      starMaxSize
+    );
+    starGeometry.setAttribute("size", new Float32BufferAttribute(starSizes, 1));
+
+    const starMaterial = createStarMaterial(starColors);
 
     const stars = new Points(starGeometry, starMaterial);
     scene.add(stars);
-    scene.add(stars);
+
+    const raycaster = new Raycaster();
+    const mouse = new Vector2();
+
+    const focusOnObject = (object: Mesh) => {
+      camera.position.set(
+        object.position.x,
+        object.position.y,
+        object.position.z + 3
+      );
+      controls.target.copy(object.position);
+    };
+
+    const handleMouseMove = (event: MouseEvent) => {
+      // Prevent the default context menu from showing up on right-click
+      if (event.button === 2) {
+        event.preventDefault();
+      }
+
+      mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+      mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+      raycaster.setFromCamera(mouse, camera);
+      const intersects = raycaster.intersectObjects([earth, moon]);
+
+      if (intersects.length > 0 && intersects[0]) {
+        setHoveredObject(intersects[0].object as Mesh);
+        if (event.button === 2) {
+          setSelectedObject(intersects[0].object as Mesh);
+        }
+      } else {
+        setHoveredObject(null);
+        if (event.button === 2) {
+          setSelectedObject(null);
+        }
+      }
+    };
+
+    const handleDoubleClick = (event: MouseEvent) => {
+      event.preventDefault();
+      raycaster.setFromCamera(mouse, camera);
+      const intersects = raycaster.intersectObjects([earth, moon]);
+
+      if (intersects.length > 0 && intersects[0]) {
+        if (intersects[0].object === earth) {
+          focusOnObject(earth);
+        } else if (intersects[0].object === moon) {
+          focusOnObject(moon);
+        }
+      }
+    };
+
+    window.addEventListener("mousedown", handleMouseMove);
+    containerRef.current.addEventListener("dblclick", handleDoubleClick);
 
     const ambientLight = new AmbientLight(0x404040);
     scene.add(ambientLight);
@@ -225,8 +344,8 @@ const Earth = () => {
         0,
         Math.min(1, (distance - thresholdDistance) / 2)
       );
-      updateEarthTextStyle({ opacity: newOpacity });
-      updateEarthComingSoonStyle({ opacity: newOpacity });
+      earthTextRef.current!.style.opacity = newOpacity.toString();
+      earthComingSoonRef.current!.style.opacity = newOpacity.toString();
     };
 
     window.addEventListener("wheel", handleWheel);
@@ -234,6 +353,13 @@ const Earth = () => {
     // Add an animation loop
     const animate = () => {
       requestAnimationFrame(animate);
+      raycaster.setFromCamera(mouse, camera);
+
+      if (cameraTarget === "earth") {
+        controls.target.copy(earth.position);
+      } else if (cameraTarget === "moon") {
+        controls.target.copy(moon.position);
+      }
 
       // Rotate the Earth and clouds
       if (!paused) {
@@ -253,18 +379,30 @@ const Earth = () => {
     animate();
     return () => {
       window.removeEventListener("wheel", handleWheel);
+      window.removeEventListener("mousedown", handleMouseMove);
+
+      if (containerRef.current) {
+        containerRef.current.removeEventListener("dblclick", handleDoubleClick);
+      }
     };
   }, [paused, generateRandomStarPositions, createStarMaterial]);
 
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyDown);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [handleKeyDown]);
+
   return (
     <div className="fixed inset-0">
-      <h1 className="earth-text font-earth737" style={earthTextStyle}>
+      <h1 ref={earthTextRef} className="earth-text no-select font-earth737">
         Earth 737
-      </h1>
+      </h1>{" "}
       <div ref={containerRef} className="h-full w-full" />
       <h1
-        className="earth-coming-soon font-earth737"
-        style={earthComingSoonStyle}
+        ref={earthComingSoonRef}
+        className="earth-coming-soon no-select  font-earth737"
       >
         Coming soon!
       </h1>
@@ -273,6 +411,7 @@ const Earth = () => {
           <AiOutlinePause className="text-4xl text-white" />
         </div>
       )}
+      {loading && <Loader />}
     </div>
   );
 };
