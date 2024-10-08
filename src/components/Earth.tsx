72a780adbebde6cbc33e1872c7cfca7d3ee59ed1
Reallisticus <jmakin64@gmail.com>
2024-10-08 13:09:56 +0300

    fxs

--- src/components/Earth.tsx
+++ src/components/Earth.tsx
@@ -1,90 +1,65 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import * as THREE from "three";
-
-import ThreeGlobe from "three-globe";
-
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-
 import countries from "../../public/custom.geo.json";
 import lines from "../../public/lines.json";
 import map from "../../public/map.json";
 
 const Earth = () => {
+  const containerRef = useRef<HTMLDivElement | null>(null);
+
   useEffect(() => {
-    let renderer: THREE.WebGLRenderer;
-    let camera: THREE.PerspectiveCamera;
-    let scene: THREE.Scene;
-    let controls;
+    let renderer: THREE.WebGLRenderer | null = null;
+    let camera: THREE.PerspectiveCamera | null = null;
+    let scene: THREE.Scene | null = null;
+    let controls: OrbitControls | null = null;
+    let Globe: any = null;
 
     let mouseX = 0;
     let mouseY = 0;
     let windowHalfX = window.innerWidth / 2;
     let windowHalfY = window.innerHeight / 2;
-    let Globe: ThreeGlobe;
 
-    init();
-    initGlobe();
-    onWindowResize();
-    animate();
-
-    function init() {
+    const init = () => {
       renderer = new THREE.WebGLRenderer({ antialias: true });
       renderer.setPixelRatio(window.devicePixelRatio);
       renderer.setSize(window.innerWidth, window.innerHeight);
-      document.body.appendChild(renderer.domElement);
+      containerRef.current?.appendChild(renderer.domElement);
 
       scene = new THREE.Scene();
-
-      var ambientLight = new THREE.AmbientLight(0xbbbbbb, 0.3);
-      scene.add(ambientLight);
       scene.background = new THREE.Color(0x040d21);
 
-      camera = new THREE.PerspectiveCamera();
-      camera.aspect = window.innerWidth / window.innerHeight;
-      camera.updateProjectionMatrix();
+      const ambientLight = new THREE.AmbientLight(0xbbbbbb, 0.3);
+      scene.add(ambientLight);
 
-      var dLight = new THREE.DirectionalLight(0xffffff, 0.8);
+      camera = new THREE.PerspectiveCamera(
+        75,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        2000
+      );
+      camera.position.set(0, 0, 400);
+
+      const dLight = new THREE.DirectionalLight(0xffffff, 0.8);
       dLight.position.set(-800, 2000, 400);
       camera.add(dLight);
 
-      var dLight1 = new THREE.DirectionalLight(0x7982f6, 1);
-      dLight1.position.set(-200, 500, 200);
-      camera.add(dLight1);
-
-      var dLight2 = new THREE.PointLight(0x8566cc, 0.5);
-      dLight2.position.set(-200, 500, 200);
-      camera.add(dLight2);
-
-      camera.position.z = 400;
-      camera.position.x = 0;
-      camera.position.y = 0;
-
       scene.add(camera);
-
-      scene.fog = new THREE.Fog(0x535ef3, 400, 2000);
 
       controls = new OrbitControls(camera, renderer.domElement);
       controls.enableDamping = true;
       controls.enablePan = false;
       controls.minDistance = 200;
       controls.maxDistance = 500;
-      controls.rotateSpeed = 0.8;
-      controls.zoomSpeed = 1;
-      controls.autoRotate = false;
 
-      controls.minPolarAngle = Math.PI / 3.5;
-      controls.maxPolarAngle = Math.PI - Math.PI / 3;
-
-      window.addEventListener("resize", onWindowResize, false);
+      window.addEventListener("resize", onWindowResize);
       document.addEventListener("mousemove", onMouseMove);
-    }
+    };
 
-    function initGlobe() {
-      Globe = new ThreeGlobe({
-        waitForGlobeReady: true,
-        animateIn: true,
-      })
+    const initGlobe = async () => {
+      const ThreeGlobe = (await import("three-globe")).default;
 
+      Globe = new ThreeGlobe()
         .hexPolygonsData(countries.features)
         .hexPolygonResolution(3)
         .hexPolygonMargin(0.7)
@@ -94,25 +69,18 @@ const Earth = () => {
 
       setTimeout(() => {
         Globe.arcsData(lines.pulls)
-          .arcColor((e) => {
-            return e.status ? "#9cff00" : "#ff4000";
-          })
-          .arcAltitude((e) => {
-            return e.arcAlt;
-          })
-          .arcStroke((e) => {
-            return e.status ? 0.5 : 0.3;
-          })
+          .arcColor((e: any) => (e.status ? "#9cff00" : "#ff4000"))
+          .arcAltitude((e: any) => e.arcAlt)
+          .arcStroke((e: any) => (e.status ? 0.5 : 0.3))
           .arcDashLength(0.9)
           .arcDashGap(4)
           .arcDashAnimateTime(1000)
           .arcsTransitionDuration(1000)
-          .arcDashInitialGap((e) => e.order * 1)
+          .arcDashInitialGap((e: any) => e.order * 1)
           .labelsData(map.maps)
           .labelColor(() => "#ffcb21")
-
           .labelDotRadius(0.3)
-          .labelSize((e) => e.size)
+          .labelSize((e: any) => e.size)
           .labelText("city")
           .labelResolution(6)
           .labelAltitude(0.01)
@@ -127,41 +95,58 @@ const Earth = () => {
       Globe.rotateZ(-Math.PI / 6);
       const globeMaterial = Globe.globeMaterial();
       globeMaterial.color = new THREE.Color(0x3a228a);
-
       globeMaterial.emissive = new THREE.Color(0x220038);
       globeMaterial.emissiveIntensity = 0.1;
       globeMaterial.shininess = 0.7;
 
-      scene.add(Globe);
-    }
+      scene!.add(Globe);
+    };
 
-    function onMouseMove(event) {
+    const onWindowResize = () => {
+      if (camera && renderer) {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+      }
+    };
+
+    const onMouseMove = (event: MouseEvent) => {
       mouseX = event.clientX - windowHalfX;
       mouseY = event.clientY - windowHalfY;
-    }
+    };
 
-    function onWindowResize() {
-      camera.aspect = window.innerWidth / window.innerHeight;
-      camera.updateProjectionMatrix();
-      windowHalfX = window.innerWidth / 1.5;
-      windowHalfY = window.innerHeight / 1.5;
-      renderer.setSize(window.innerWidth, window.innerHeight);
-    }
+    const animate = () => {
+      if (camera && controls && renderer && scene) {
+        camera.position.x +=
+          Math.abs(mouseX) <= windowHalfX / 2
+            ? (mouseX / 2 - camera.position.x) * 0.005
+            : 0;
+        camera.position.y += (-mouseY / 2 - camera.position.y) * 0.005;
+        camera.lookAt(scene.position);
+        controls.update();
+        renderer.render(scene, camera);
+        requestAnimationFrame(animate);
+      }
+    };
 
-    function animate() {
-      camera.position.x +=
-        Math.abs(mouseX) <= windowHalfX / 2
-          ? (mouseX / 2 - camera.position.x) * 0.005
-          : 0;
-      camera.position.y += (-mouseY / 2 - camera.position.y) * 0.005;
-      camera.lookAt(scene.position);
-      controls.update();
-      renderer.render(scene, camera);
-      requestAnimationFrame(animate);
-    }
-  });
+    // Initialization and animation
+    init();
+    initGlobe();
+    animate();
 
-  return <div>Earth</div>;
+    // Cleanup
+    return () => {
+      if (renderer) {
+        containerRef.current?.removeChild(renderer.domElement);
+      }
+      window.removeEventListener("resize", onWindowResize);
+      document.removeEventListener("mousemove", onMouseMove);
+      if (controls) controls.dispose();
+      if (scene) scene.clear();
+    };
+  }, []);
+
+  return <div id="earth-container" ref={containerRef}></div>;
 };
 
 export default Earth;
